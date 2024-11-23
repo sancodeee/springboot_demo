ec17ba7271d63c02944dc2d87a99ae8484e1535e
sancodeee <wangsen715@gmail.com>
2024-11-23 16:22:00 +0800

    feat: 注释

--- src/main/java/com/ws/rabbitmq/producer/ProducerOneService.java
+++ src/main/java/com/ws/rabbitmq/producer/ProducerOneService.java
@@ -1,4 +1,10 @@
 package com.ws.rabbitmq.producer;
 
+/**
+ * 生产者服务一
+ *
+ * @author wangsen
+ * @date 2024/11/23
+ */
 public interface ProducerOneService {
 }
--- src/main/java/com/ws/threadpool/service/threadPoolService.java
+++ src/main/java/com/ws/threadpool/service/threadPoolService.java
@@ -1,4 +1,10 @@
 package com.ws.threadpool.service;
 
+/**
+ * 线程池服务
+ *
+ * @author wangsen
+ * @date 2024/11/23
+ */
 public interface threadPoolService {
 }
